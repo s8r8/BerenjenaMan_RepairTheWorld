5618be2eabef52df41a5434e9a13e22449af0eea
s8r8 <sara.ventas.arguello@gmail.com>
2020-02-01 17:35:46 +0100

    Forest

---
+++ Berenjeno/Assets/camaracontr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaracontr : MonoBehaviour
+{
+    public GameObject personaje;
+    private Vector3 posicion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicion = transform.position - personaje.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = personaje.transform.position + posicion;
+    }
+}
+
